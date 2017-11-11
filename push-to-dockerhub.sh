4fbd130ae19ffdf7bfd963c87cb29b315ad88895
Anastasios Zouzias <zouzias@gmail.com>
2017-11-11 19:55:36 +0100

    script:push to dockerhub

---
+++ push-to-dockerhub.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage: ./build-docker BOOST_VERSION, i.e., 1.65.1"
+    exit
+fi
+
+./build-docker.sh $1
+docker login
+docker push zouzias/boost:$1
